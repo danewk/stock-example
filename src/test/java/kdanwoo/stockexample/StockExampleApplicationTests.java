1199cc6d72a8b594a8f1452e2ee0a586d27f5ccd
kdanwoo <kdanwoo@jobis.co>
2022-08-30 23:29:14 +0900

    재고시스템으로 알아보는 동시성이슈 해결방법

---
+++ src/test/java/kdanwoo/stockexample/StockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kdanwoo.stockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
